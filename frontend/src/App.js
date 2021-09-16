4299eb6c957ba52dc677489d567805f58943a8f7
Lucascfsb <lucascfsb@gmail.com>
2021-09-15 21:22:39 -0300

    Linking the front-end with the back-end
    Linking the front-end with the back-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+
+import api from './services/api';
 
 import './App.css';
-
-import backgroundImage from './assets/background.jpg';
 
 import Header from './components/header';
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Fron-end web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(res => {
+      setProjects(res.data)
+    });
+  }, []);
 
   function handleAddProject() {
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -19,10 +25,8 @@ function App() {
     <>
       <Header title= "Projects"/>
 
-      <img width={300} src={backgroundImage}/>
-
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type ="button" onClick={handleAddProject}>Adicionar projeto</button>
